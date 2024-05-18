990e196b6453f20ca46284f420fc472402b6693a
Drawls001 <97949349+Drawls001@users.noreply.github.com>
2024-05-18 19:57:47 +0200

    Version 0.0.1

---
+++ Version1_/includes/shop.php
@@ -0,0 +1,9 @@
+<div class="card m-5" style="width: 13rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text"> %CARD LABEL% </p>
+    <label class="label mx-auto" name="price">$0.00</label>
+    <a action="post" class="btn btn-success ">In cart</a>
+  </div>
+</div>
